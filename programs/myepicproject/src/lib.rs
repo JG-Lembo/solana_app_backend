d960d2dfed4c450b035b6bebc349dece2aa92d0e
João Gabriel Lembo <joaogabriellembo@usp.br>
2023-06-03 17:30:43 -0300

    Adiciona total de gifs

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -6,9 +6,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+  
+	// Outra função uhul!
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    // Obtem a referencia para a conta e incrementa total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
     Ok(())
   }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+// Especifica que dados queremos no Contexto AddGif
+// Obtendo um controle sobre o fluxo das coisas 😊?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,37 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Iniciando testes...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
+  const program = anchor.workspace.Myepicproject;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
   console.log("📝 Sua assinatura de transação", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+	
+  // Chama add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+  
+  // Obtem a conta novamente e veja o que mudou.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
